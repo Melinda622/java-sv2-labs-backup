e9fa95c2615a70959fb1a132334205fae6e79829
Melinda622 <melindasahouli@hotmail.com>
2022-01-16 19:52:48 +0100

    Book.java and Library.java created;

---
+++ training-solutions/src/main/java/genericsusage/withoutgenerics/Library.java
@@ -0,0 +1,19 @@
+package genericsusage.withoutgenerics;
+
+import genericsusage.Book;
+
+import java.util.List;
+
+public class Library {
+
+    public Book getFirstBook(List books){
+        if(books==null){
+            throw new NullPointerException("Argument should not be null!");
+        }else if(books.isEmpty()){
+            throw new IllegalArgumentException("Argument should not be empty!");
+        }else if(!(books.get(0) instanceof Book)){
+            throw new ClassCastException("Not a book");
+        }
+        return (Book) books.get(0);
+    }
+}
