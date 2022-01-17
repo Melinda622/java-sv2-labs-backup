66dc7260b254309324c4b3c0e058a92010c74e7c
Melinda622 <melindasahouli@hotmail.com>
2022-01-18 00:25:44 +0100

    equals() and HashCode() added to Exam.java;

--- training-solutions/src/main/java/collectionsclass/ExamResult.java
+++ training-solutions/src/main/java/collectionsclass/ExamResult.java
@@ -1,5 +1,7 @@
 package collectionsclass;
 
+import java.util.Objects;
+
 public class ExamResult implements Comparable<ExamResult> {
 
     private String name;
@@ -13,6 +15,19 @@ public class ExamResult implements Comparable<ExamResult> {
     @Override
     public int compareTo(ExamResult e) {
         return this.result - e.result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExamResult that = (ExamResult) o;
+        return result == that.result;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(result);
     }
 
     public String getName() {
