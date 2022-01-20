38a44b2959f86f2fdd19a71ab674756a206c26aa
Melinda622 <melindasahouli@hotmail.com>
2022-01-20 21:05:29 +0100

    Cake.java and CookBook.java created;

---
+++ training-solutions/src/main/java/streams/CookBook.java
@@ -0,0 +1,29 @@
+package streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CookBook {
+
+    private List<Cake> cakes = new ArrayList<>();
+
+    public CookBook(List<Cake> cakes) {
+        this.cakes = cakes;
+    }
+
+    public List<Cake> getCakes() {
+        return new ArrayList<>(cakes);
+    }
+
+    public List<String> listCakeNamesWithGivenIngredient(String ingredient) {
+        return cakes.stream().filter(cake -> cake.getIngredients().contains(ingredient)).map(cake -> cake.getName()).
+                collect(Collectors.toList());
+    }
+
+    public List<String> listCakeNamesWithMaxIngredients(int max) {
+        List<String> names = cakes.stream().filter(cake -> cake.getIngredients().size() <= max).
+                map(cake -> cake.getName()).collect(Collectors.toList());
+        return names;
+    }
+}
