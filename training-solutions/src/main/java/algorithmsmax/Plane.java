b102c51e8e8d4a3882de243456ec647d841544b0
Melinda622 <melindasahouli@hotmail.com>
2022-01-27 13:21:04 +0100

    Plane.java completed;

--- training-solutions/src/main/java/algorithmsmax/Plane.java
+++ training-solutions/src/main/java/algorithmsmax/Plane.java
@@ -1,16 +1,27 @@
 package algorithmsmax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plane {
 
-   /* public static void main(String[] args) {
-        String s="1000000011111";
-        char[] numbers=s.toCharArray();
-        int count=0;
-        for (int i = 1; i <numbers.length ; i++) {
-            if(numbers[i]=='1'&&numbers[i+1]=='1'||numbers[i]=='1'&&numbers[i-1]=='1'){
-                count++;
-            }
-        } System.out.println(count);
+    public int getLongestOcean(String map) {
+        int count = 0;
+        List<Integer> oceans = new ArrayList<>();
+        int lengthOfOcean=0;
+        int max = 0;
 
-    }*/
+        for (int i = 0; i < map.length() - 1; i++) {
+            if (map.charAt(i) == '0' && map.charAt(i + 1) == '0') {
+                count++;
+            }else if (map.charAt(i + 1) != '0' && count > 0) {
+                oceans.add(count + 1);
+                lengthOfOcean=count+1;
+                count = 0;
+            }else if(lengthOfOcean>max){
+                max=lengthOfOcean;
+            }
+        }
+        return max;
+    }
 }
